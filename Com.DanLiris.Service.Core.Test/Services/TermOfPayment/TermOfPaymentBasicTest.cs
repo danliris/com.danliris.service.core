d540a6346d767f18cfa4d2393f07b9d1e488db7f
sama08 <usamah_alan@hotmail.com>
2019-12-24 10:39:22 +0700

    Term Of Payment test

---
+++ Com.DanLiris.Service.Core.Test/Services/TermOfPayment/TermOfPaymentBasicTest.cs
@@ -0,0 +1,45 @@
+﻿using Com.Danliris.Service.Core.Test.Helpers;
+using Com.DanLiris.Service.Core.Lib;
+using Com.DanLiris.Service.Core.Lib.Models;
+using Com.DanLiris.Service.Core.Lib.Services;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.DanLiris.Service.Core.Test.Services.TermOfPaymentTest
+{
+    [Collection("ServiceProviderFixture Collection")]
+    public class TermOfPaymentBasicTest : BasicServiceTest<CoreDbContext, TermOfPaymentService, TermOfPayment>
+    {
+        private static readonly string[] createAttrAssertions = { "Name", "Code" };
+        private static readonly string[] updateAttrAssertions = { "Name", "Code" };
+        private static readonly string[] existAttrCriteria = { "Code" };
+
+        public TermOfPaymentBasicTest(ServiceProviderFixture fixture) : base(fixture, createAttrAssertions, updateAttrAssertions, existAttrCriteria)
+        {
+        }
+
+        public override void EmptyCreateModel(TermOfPayment model)
+        {
+            model.Code = string.Empty;
+            model.Name = string.Empty;
+        }
+
+        public override void EmptyUpdateModel(TermOfPayment model)
+        {
+            model.Code = string.Empty;
+            model.Name = string.Empty;
+        }
+
+        public override TermOfPayment GenerateTestModel()
+        {
+            string guid = Guid.NewGuid().ToString();
+
+            return new TermOfPayment
+            {
+                Code = string.Format("TermOfPaymentCode {0}", guid),
+                Name = string.Format("TermOfPaymentName {0}", guid),
+            };
+        }
+    }
+}
