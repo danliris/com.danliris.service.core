d679932941f9ccf1b3ff66ad9e5743ee139a3459
sama08 <usamah_alan@hotmail.com>
2019-12-24 10:49:48 +0700

    Revert "Merge remote-tracking branch 'upstream/dev' into dev"
    This reverts commit f42b8b016a19037c3361b5dd9481eb530b897966, reversing
    changes made to 5e26cb7afd884d1bb6227fee69a82b293cf317b1.

--- Com.DanLiris.Service.Core.Lib/Services/BuyerService.cs
+++ Com.DanLiris.Service.Core.Lib/Services/BuyerService.cs
@@ -46,7 +46,7 @@ namespace Com.DanLiris.Service.Core.Lib.Services
             /* Const Select */
             List<string> SelectedFields = new List<string>()
             {
-                "Id", "Code", "Name", "Address", "City", "Country", "Contact", "Tempo", "_LastModifiedUtc", "Type", "NPWP"
+                "Id", "Code", "Name", "Address", "City", "Country", "Contact", "Tempo", "_LastModifiedUtc", "Type"
             };
 
             Query = Query
@@ -61,7 +61,6 @@ namespace Com.DanLiris.Service.Core.Lib.Services
                     Contact = b.Contact,
                     Tempo = b.Tempo,
                     Type = b.Type,
-                    NPWP = b. NPWP,
                     _LastModifiedUtc = b._LastModifiedUtc
                 });
 
