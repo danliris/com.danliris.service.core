b8453e44a9b97481c011affcecf04174580b315d
sama08 <usamah_alan@hotmail.com>
2019-12-26 13:25:17 +0700

    Update TermOfPaymentBasicTest.cs

--- Com.DanLiris.Service.Core.Test/Controllers/TermOfPayment/TermOfPaymentBasicTest.cs
+++ Com.DanLiris.Service.Core.Test/Controllers/TermOfPayment/TermOfPaymentBasicTest.cs
@@ -15,10 +15,10 @@ namespace Com.DanLiris.Service.Core.Test.Controllers.TermOfPaymentTest
     [Collection("TestFixture Collection")]
     public class TermOfPaymentBasicTest : BasicControllerTest<CoreDbContext, TermOfPaymentService, TermOfPayment, TermOfPaymentViewModel, TermOfPaymentDataUtil>
     {
-        private const string URI = "v1/master/storages";
+        private const string URI = "v1/master/term-of-payments";
 
-        private static List<string> CreateValidationAttributes = new List<string> { };
-        private static List<string> UpdateValidationAttributes = new List<string> { };
+        private static List<string> CreateValidationAttributes = new List<string> { "Name", "Code" };
+        private static List<string> UpdateValidationAttributes = new List<string> { "Name", "Code" };
 
         public TermOfPaymentBasicTest(TestServerFixture fixture) : base(fixture, URI, CreateValidationAttributes, UpdateValidationAttributes)
         {
