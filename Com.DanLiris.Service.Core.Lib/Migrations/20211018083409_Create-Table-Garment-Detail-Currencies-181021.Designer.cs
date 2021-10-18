a56f5433f54406a48918186eff7806d22aac81c8
AndreaZain <andrea.kinan@gmail.com>
2021-10-18 16:26:23 +0700

    Create Table Detail Currencies
    Create Table Detail Currencies

--- Com.DanLiris.Service.Core.Lib/Migrations/20211018083409_Create-Table-Garment-Detail-Currencies-181021.Designer.cs
+++ Com.DanLiris.Service.Core.Lib/Migrations/20211018083409_Create-Table-Garment-Detail-Currencies-181021.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Com.DanLiris.Service.Core.Lib.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211018083409_Create-Table-Garment-Detail-Currencies-181021")]
+    partial class CreateTableGarmentDetailCurrencies181021
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
