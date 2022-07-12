d47b4436d7dce00392a0897d1e8268788a9f97da
AndreaZain <andrea.kinan@gmail.com>
2022-07-12 08:40:43 +0700

    Enhance - Detail Currency
    Fix Service (offset)

--- Com.DanLiris.Service.Core.Lib/Services/GarmentDetailCurrencyService.cs
+++ Com.DanLiris.Service.Core.Lib/Services/GarmentDetailCurrencyService.cs
@@ -120,8 +120,8 @@ namespace Com.DanLiris.Service.Core.Lib.Services
             garmentCurrency._LastModifiedBy = garmentCurrencyVM._LastModifiedBy;
             garmentCurrency._LastModifiedAgent = garmentCurrencyVM._LastModifiedAgent;
             garmentCurrency.Code = garmentCurrencyVM.code;
-            garmentCurrency.Date = garmentCurrencyVM.date;
-            garmentCurrency.Rate = garmentCurrencyVM.rate;
+            garmentCurrency.Date = garmentCurrencyVM.date.AddHours(7);
+            garmentCurrency.Rate = garmentCurrencyVM.rate; 
             
             return garmentCurrency;
         }
