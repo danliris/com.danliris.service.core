a02af40b22dd3d2333bbf5833b8bfb827b9eae38
ardana96 <ardanagama@gmail.com>
2022-10-17 16:46:29 +0700

    Add Product Textile

---
+++ Com.DanLiris.Service.Core.Lib/Migrations/20221017062451_Add_Product_Textiles.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Core.Lib.Migrations
+{
+    public partial class Add_Product_Textiles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductTextiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    Code = table.Column<string>(maxLength: 255, nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(maxLength: 255, nullable: true),
+                    UomId = table.Column<int>(nullable: true),
+                    UomUnit = table.Column<string>(maxLength: 255, nullable: true),
+                    _CreatedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    _CreatedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    _CreatedUtc = table.Column<DateTime>(nullable: false),
+                    _DeletedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    _DeletedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    _DeletedUtc = table.Column<DateTime>(nullable: false),
+                    _IsDeleted = table.Column<bool>(nullable: false),
+                    _LastModifiedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    _LastModifiedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    _LastModifiedUtc = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTextiles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductTextiles");
+        }
+    }
+}
---
+++ Com.DanLiris.Service.Core.Lib/Models/ProductTextile.cs
@@ -0,0 +1,26 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Com.DanLiris.Service.Core.Lib.Models
+{
+    public class ProductTextile : StandardEntity, IValidatableObject
+    {
+        [StringLength(255)]
+        public string Code { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
+        public int? UomId { get; set; }
+        [StringLength(255)]
+        public string UomUnit { get; set; }
+        public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> validationResult = new List<ValidationResult>();
+            return validationResult;
+        }
+    }
+}
