887bfa0097df76d2fc3cae305f1f27e042df9bad
ardana96 <ardanagama@gmail.com>
2022-11-01 16:38:21 +0700

    Enhancement Product Textile buyer Type

---
+++ Com.DanLiris.Service.Core.Lib/Migrations/20221101020810_add_column_buyer_type_product_textile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Core.Lib.Migrations
+{
+    public partial class add_column_buyer_type_product_textile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerType",
+                table: "ProductTextiles",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyerType",
+                table: "ProductTextiles");
+        }
+    }
+}
