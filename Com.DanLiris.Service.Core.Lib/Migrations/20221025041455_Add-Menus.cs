001eb939fe9e5ef13a7c703ede95acad9b491e3c
ardana96 <ardanagama@gmail.com>
2022-12-02 18:22:32 +0700

    Enhancement Menu Auth

---
+++ Com.DanLiris.Service.Core.Lib/Migrations/20221025041455_Add-Menus.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Core.Lib.Migrations
+{
+    public partial class AddMenus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    Code = table.Column<string>(maxLength: 100, nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Menu = table.Column<string>(maxLength: 500, nullable: true),
+                    MenuName = table.Column<string>(maxLength: 500, nullable: true),
+                    SubMenu = table.Column<string>(maxLength: 500, nullable: true),
+                    UId = table.Column<string>(maxLength: 255, nullable: true),
+                    _CreatedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    _CreatedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    _CreatedUtc = table.Column<DateTime>(nullable: false),
+                    _DeletedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    _DeletedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    _DeletedUtc = table.Column<DateTime>(nullable: false),
+                    _IsDeleted = table.Column<bool>(nullable: false),
+                    _LastModifiedAgent = table.Column<string>(maxLength: 255, nullable: false),
+                    _LastModifiedBy = table.Column<string>(maxLength: 255, nullable: false),
+                    _LastModifiedUtc = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
