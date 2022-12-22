95d0ff642f3cf3ef16566aad834724495e16ba36
AndreaZain <andrea.kinan@gmail.com>
2022-12-22 14:24:13 +0700

    Create Garment Marketing
    Create Garment Marketing For CCG

--- Com.DanLiris.Service.Core.Lib/CoreDbContext.cs
+++ Com.DanLiris.Service.Core.Lib/CoreDbContext.cs
@@ -96,6 +96,7 @@ namespace Com.DanLiris.Service.Core.Lib
         public DbSet<GarmentWareHouseModel> GarmentWareHouses { get; set; }
         public DbSet<ProductTextile> ProductTextiles { get; set; }
         public DbSet<Menus> Menus { get; set; }
+        public DbSet<GarmentMarketingModel> GarmentMarketings { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ Com.DanLiris.Service.Core.Test/Controllers/GarmentMarketing/BasicTest.cs
@@ -0,0 +1,15 @@
+﻿using Com.DanLiris.Service.Core.Lib.Models;
+using Com.DanLiris.Service.Core.Lib.Services.GarmentMarketing;
+using Com.DanLiris.Service.Core.Lib.ViewModels;
+using Com.DanLiris.Service.Core.Test.Utils;
+using Com.DanLiris.Service.Core.WebApi.Controllers.v1.BasicControllers;
+
+namespace Com.DanLiris.Service.Core.Test.Controllers.GarmentMarketing
+{
+    public class BasicTest : BaseControllerTest<GarmentMarketingController, GarmentMarketingModel, GarmentMarketingViewModel, IGarmentMarketingService>
+    {
+        public BasicTest()
+        {
+        }
+    }
+}
