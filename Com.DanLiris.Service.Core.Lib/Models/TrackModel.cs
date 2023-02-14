1b17dd9f9dd38fa38addb7314cf5e84f9f92b65c
ardana96 <ardanagama@gmail.com>
2023-02-14 08:44:47 +0700

    Add Master Track

---
+++ Com.DanLiris.Service.Core.Lib/Models/TrackModel.cs
@@ -0,0 +1,28 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Com.DanLiris.Service.Core.Lib.Models
+{
+    public class TrackModel :StandardEntity, IValidatableObject
+    {
+        [StringLength(255)]
+        public string Type { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> validationResult = new List<ValidationResult>();
+
+            if (string.IsNullOrWhiteSpace(this.Type))
+                validationResult.Add(new ValidationResult("Type is required", new List<string> { "type" }));
+
+            if (string.IsNullOrWhiteSpace(this.Name))
+                validationResult.Add(new ValidationResult("Name is required", new List<string> { "name" }));
+
+            return validationResult;
+        }
+    }
+}
