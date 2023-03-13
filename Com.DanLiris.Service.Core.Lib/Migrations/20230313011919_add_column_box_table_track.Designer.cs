d6dbd131857ccabe58776185d69761377fb57dff
ardana96 <ardanagama@gmail.com>
2023-03-13 09:56:32 +0700

    Stock Opname - Enhancement Master Track

--- Com.DanLiris.Service.Core.Lib/Migrations/20230313011919_add_column_box_table_track.Designer.cs
+++ Com.DanLiris.Service.Core.Lib/Migrations/20230313011919_add_column_box_table_track.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Com.DanLiris.Service.Core.Lib.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230313011919_add_column_box_table_track")]
+    partial class add_column_box_table_track
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Com.DanLiris.Service.Core.Lib/Models/TrackModel.cs
+++ Com.DanLiris.Service.Core.Lib/Models/TrackModel.cs
@@ -14,6 +14,7 @@ namespace Com.DanLiris.Service.Core.Lib.Models
         public string Type { get; set; }
         [StringLength(255)]
         public string Name { get; set; }
+        public string Box { get; set; }
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             List<ValidationResult> validationResult = new List<ValidationResult>();
@@ -23,13 +24,16 @@ namespace Com.DanLiris.Service.Core.Lib.Models
 
             if (string.IsNullOrWhiteSpace(this.Name))
                 validationResult.Add(new ValidationResult("Name is required", new List<string> { "name" }));
-            
+
+            if (string.IsNullOrWhiteSpace(this.Box))
+                validationResult.Add(new ValidationResult("Box is required", new List<string> { "box" }));
+
             if (validationResult.Count.Equals(0))
             {
                 /* Service Validation */
                 TrackService service = (TrackService)validationContext.GetService(typeof(TrackService));
 
-                if (service.DbContext.Set<TrackModel>().Count(r => r._IsDeleted.Equals(false) && r.Type == this.Type && r.Name.Equals(this.Name)) > 0) /* Code Unique */
+                if (service.DbContext.Set<TrackModel>().Count(r => r._IsDeleted.Equals(false) && r.Type == this.Type && r.Name.Equals(this.Name) && r.Box.Equals(this.Box)) > 0) /* Code Unique */
                     validationResult.Add(new ValidationResult("Code already exists", new List<string> { "track" }));
             }
             return validationResult;
