176c12c1a705e60d389f6e85a19e34fd6be0f458
ardana96 <ardanagama@gmail.com>
2023-08-14 08:37:28 +0700

    Enhancement Monitoring Product Price

---
+++ Com.DanLiris.Service.Core.Lib/Migrations/20230811010941_add_column_datebefore.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Core.Lib.Migrations
+{
+    public partial class add_column_datebefore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateBefore",
+                table: "ProductPriceHistories",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateBefore",
+                table: "ProductPriceHistories");
+        }
+    }
+}
