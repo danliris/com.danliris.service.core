4d5ba978b048805881e8b476acac193f894d3ab3
AndreaZain <andrea.kinan@gmail.com>
2024-10-11 16:40:31 +0700

    Enahnce GMt Supplier
    Add Description
    Add Download Function

---
+++ Com.DanLiris.Service.Core.Lib/Migrations/20241011014952_Add-Column-Descriptoion-Table-Garment-Suppliers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.DanLiris.Service.Core.Lib.Migrations
+{
+    public partial class AddColumnDescriptoionTableGarmentSuppliers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "GarmentSuppliers",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "GarmentSuppliers");
+        }
+    }
+}
